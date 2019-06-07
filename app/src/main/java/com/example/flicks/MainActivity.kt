4e53cc5fa4638fade697ff1cb33183ed71aaca07
Joshua Mugisha <joshuamugisha@MacBook-Pro-90.local>
2019-06-07 10:27:43 +0300

    add navigation up-button

--- app/src/main/java/com/example/flicks/MainActivity.kt
+++ app/src/main/java/com/example/flicks/MainActivity.kt
@@ -3,18 +3,24 @@ package com.example.flicks
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.StrictMode
-
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
+import com.example.flicks.databinding.ActivityMainBinding
 
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        @Suppress("UNUSED_VARIABLE")
+        val binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
+        val navController = this.findNavController(R.id.nav_host_fragment)
+        NavigationUI.setupActionBarWithNavController(this, navController)
+    }
 
-//        if (android.os.Build.VERSION.SDK_INT > 9) {
-//            val policy = StrictMode.ThreadPolicy.Builder().permitAll().build()
-//            StrictMode.setThreadPolicy(policy)
-//        }
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = this.findNavController(R.id.nav_host_fragment)
+        return navController.navigateUp()
     }
 }
