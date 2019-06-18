f1df2d5f16459d9b41f0ecebab7be011b2edf989
Joshua Mugisha <joshuamugisha@MacBook-Pro-90.local>
2019-06-18 14:20:22 +0300

    add favourites movies tab and all movies tab on the bottom navigation

--- app/src/main/java/com/example/flicks/overview/FavouritesOverviewFragment.kt
+++ app/src/main/java/com/example/flicks/overview/FavouritesOverviewFragment.kt
@@ -1,22 +1,18 @@
 package com.example.flicks.overview
 
 import android.os.Bundle
-import android.view.*
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import androidx.navigation.fragment.findNavController
-import com.example.flicks.R
 import com.example.flicks.database.MovieDatabase
-import com.example.flicks.databinding.FragmentMoviesBinding
-import com.example.flicks.network.MovieApiFilter
+import com.example.flicks.databinding.FragmentFavouritesBinding
 
 class FavouritesOverviewFragment : Fragment() {
     lateinit var viewModel: OverviewViewModel
-
-//    private val viewModel: OverviewViewModel by lazy {
-//        ViewModelProviders.of(this).get(OverviewViewModel::class.java)
-//    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -24,35 +20,31 @@ class FavouritesOverviewFragment : Fragment() {
     ): View? {
 
         val application = requireNotNull(value = activity).application
-//        val binding = FragmentDetailBinding.inflate(inflater)
-
-//        val resultProperty = DetailFragmentArgs.fromBundle(arguments!!).selectedProperty
         val dataSource = MovieDatabase.getInstance(application).movieDatabaseDao()
-        val viewModelFactory = OverviewViewModelFactory(application,dataSource)
-        val binding = FragmentMoviesBinding.inflate(inflater)
+        val viewModelFactory = OverviewViewModelFactory(application, dataSource)
+        val binding = FragmentFavouritesBinding.inflate(inflater)
         viewModel = ViewModelProviders.of(
-            this, viewModelFactory).get(OverviewViewModel::class.java)
+            this, viewModelFactory
+        ).get(OverviewViewModel::class.java)
 
         binding.lifecycleOwner = this
 
         binding.viewModel = viewModel
 
-        binding.photosGrid.adapter = PhotoGridAdapter(PhotoGridAdapter.OnClickListener{
+        binding.photosGrid.adapter = PhotoGridAdapter(PhotoGridAdapter.OnClickListener {
             viewModel.displayPropertyDetails(it)
         })
         viewModel.navigateToSelectedMovie.observe(this, Observer {
-            if (null != it){
+            if (null != it) {
                 this.findNavController().navigate(OverviewFragmentDirections.actionShowDetail(it))
                 viewModel.displayPropertyDetailsComplete()
             }
         })
         viewModel.dbData.observe(this, Observer {
-            if (null != it){
+            if (null != it) {
                 viewModel.getAllMovies()
             }
         })
-
-        setHasOptionsMenu(true)
         return binding.root
     }
 }
